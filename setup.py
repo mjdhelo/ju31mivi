77019593eee4269b7918cce079f7bf5cc16e26a0
Majd Hlou <62136771+mjdhelo@users.noreply.github.com>
2021-11-14 21:43:34 +0100

    Add files via upload
    this is my first package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='ju31mivi',
+version='0.1',
+author='DSSS',
+author_email='majd.hlou@fau.de',
+packages=find_packages(),
+install_requires=['numpy', 'Pillow', 'ipywidgets'])
+
