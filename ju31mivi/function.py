b6c0852b6d4e983cb6e5487b0e64a6b268c2959f
Majd Hlou <62136771+mjdhelo@users.noreply.github.com>
2021-11-14 21:42:59 +0100

    Add files via upload
    my first package

---
+++ ju31mivi/function.py
@@ -0,0 +1,13 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
+
+def imshow(X, resize=None):
+
+    img = Image.fromarray(X)
+    if resize is not None:
+        img.resize(resize)
+    img.show()
+    return img
+    
+    
